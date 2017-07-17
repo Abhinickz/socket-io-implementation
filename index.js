f6c544743807fa8596ed2c9129cf16c2bfdff94f
Abhinickz <Abhinickz6@gmail.com>
2017-07-17 18:06:05 +0530

    Done: Broadcast a message to connected users when someone connects or disconnects

--- index.js
+++ index.js
@@ -3,25 +3,26 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 http.listen( 3000, function(){
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
 
 io.on('connection', function(socket){
-  console.log('A User Connected');
-  socket.on('chat message', function(msg){
-    console.log('Message: ' + msg);
-  });
-  socket.on('disconnect', function(){
-    console.log('User Disconnected');
-  });
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+	console.log('A User Connected');
+	io.emit('chat message', 'A User Connected');
+	socket.on('chat message', function(msg){
+		console.log('Message: ' + msg);
+	});
+	socket.on('disconnect', function(){
+		console.log('User Disconnected');
+		io.emit('chat message', 'User Disconnected');
+	});
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
 });
-
 
 

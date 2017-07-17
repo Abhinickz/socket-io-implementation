e4c276176267ba836f4766c1d3820b67cdc679a7
Abhinickz <Abhinickz6@gmail.com>
2017-07-17 19:20:38 +0530

    Adding remaining wishlist of socket.io

--- index.js
+++ index.js
@@ -13,15 +13,18 @@ http.listen( 3000, function(){
 io.on('connection', function(socket){
 	console.log('A User Connected');
 	io.emit('chat message', 'A User Connected');
-	socket.on('chat message', function(msg){
-		console.log('Message: ' + msg);
-	});
+
 	socket.on('disconnect', function(){
 		console.log('User Disconnected');
 		io.emit('chat message', 'User Disconnected');
 	});
 	socket.on('chat message', function(msg){
 		io.emit('chat message', msg);
+		console.log('Public Message: ' + msg);
+	});
+	socket.on('private message', function(msg){
+		io.emit('private message', msg);
+		console.log('Private Message: ' + msg);
 	});
 });
 
